1349f9893e1a53f8f8e3ed5ba04b4253ca311e38
Dušan Simić <dusan.simic1810@gmail.com>
2018-07-05 21:01:24 +0200

    Changes in contour finding method
    Now increasing image contrast for easier contour finding.

--- util.py
+++ util.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
 
 def findLargestContour(contours):
 	largestContour = max(contours, key = cv.contourArea)
@@ -21,17 +20,13 @@ def getThreshAndContour(img):
 
 	return (thresh, contours)
 
+def __increaseContrast(img, brightness, contrast):
+	imgInceasedContrast = cv.addWeighted(img, 1.0 + contrast / 127, img, 0, brightness - contrast)
+	return imgInceasedContrast
+
 def getContourHSV(img):
-	imghsv = cv.cvtColor(img, cv.COLOR_BGR2HSV) # Convert to HSV
-	imghsv2means = imghsv.reshape((-1, 3))
-	imghsv2means = np.float32(imghsv2means) # Prepare image for kmeans()
-	coords = cv.KMEANS_RANDOM_CENTERS
-	criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-	_, label, center = cv.kmeans(imghsv2means, 2, None, criteria, 10, coords) # Do kmeans
-	center = np.uint8(center)
-	res = center[label.flatten()]
-	imghsv2means = res.reshape((img.shape)) # Convert to usable image with 2 colors
-	imggray = cv.cvtColor(imghsv2means, cv.COLOR_BGR2GRAY) # Convert to BW
+	imgInceasedContrast = __increaseContrast(img, 64.0, 0.0)
+	imggray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # Convert to BW
 	blur = cv.GaussianBlur(imggray, (5,5), 0)
 	_, thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU) # Getting threshold
 	img2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # Getting contour
